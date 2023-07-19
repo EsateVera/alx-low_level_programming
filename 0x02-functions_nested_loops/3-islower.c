b21a0b605b85eada50db7db037149c52f1e552b2
Esatevera <veravinesate@gmail.com>
2023-07-19 02:50:53 -0700

    c description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *  _islower - Entry level
- *  Description: 'Function that checks for lowercase character'
+ *  _islower - 'Function that checks for lowercase character'
+ * @c: The character to be checked
  * Return: 1 for lower character 0 for anything else
  */
 
